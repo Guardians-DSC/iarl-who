a8a4a039ea45328b52c7cb4beea9967bd4f89556
raianyrufino <raiany.paz@ccc.ufcg.edu.br>
2019-04-04 20:55:36 -0300

    raianyrufino

---
+++ who/raianyrufino.js
@@ -0,0 +1,5 @@
+addMember({
+  name:"Raiany Rufino",
+  github:"https://github.com/raianyrufino",
+  image:"https://avatars2.githubusercontent.com/u/44267973?s=400&u=66daac29b5b21bc1a14626bfd4bcc8ba77bb628b&v=4"
+});
