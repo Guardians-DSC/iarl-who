75ebb850717c11b87aecf3c0aa0ec13eda9f3f45
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2019-04-06 13:07:29 -0300

    Update who/matheuspatriota.js
    Co-Authored-By: MatheusPatriota <32914601+MatheusPatriota@users.noreply.github.com>

--- who/matheuspatriota.js
+++ who/matheuspatriota.js
@@ -2,4 +2,4 @@ addMember({
     name: "Matheus da Silva Coimbra Patriota",
     github: "https://github.com/MatheusPatriota",
     image: "https://avatars2.githubusercontent.com/u/32914601?s=460&v=4"
-})
+});
