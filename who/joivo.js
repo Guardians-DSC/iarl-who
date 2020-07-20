feb6a6affa631df2512a4e4c7139fcfa99d379f7
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2020-07-20 13:11:43 -0300

    Update and rename emanueljoivo.js to joivo.js

--- who/joivo.js
+++ who/joivo.js
@@ -1,5 +1,5 @@
 addMember({
   name:"Emanuel Joivo",
-  github:"https://github.com/emanueljoivo",
+  github:"https://github.com/joivo",
   image:"https://avatars0.githubusercontent.com/u/20460600?s=460&v=4"
 });
