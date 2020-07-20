84d316828b17adc1500a018935cd7e62d6f88023
David Ferreira Quaresma <david.quaresma@ccc.ufcg.edu.br>
2020-07-20 13:10:57 -0300

    Update kaiokassiano.js

--- who/kaiokassiano.js
+++ who/kaiokassiano.js
@@ -1,5 +1,5 @@
 addMember({
   name:"Kaio Oliveira",
-  github:"https://github.com/kaiokassiano",
+  github:"https://github.com/kaiokmo",
   image:"https://avatars3.githubusercontent.com/u/15371403?s=460&v=4"
 });
